78ef324457ab9237654f72c50dcf1693df2c34d7
yongbo.zeng_U+ <yongbo.zeng@united-imaging.com>
2024-05-25 13:30:43 +0800

    feat: add on-demand-render tutorial

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,75 @@
-
 <template>
   <div>
-    <nav style="
-      height: 60px;
-      width: 100%;
-      padding: 0 20px;
-      background-color: #333;
-      color: white;
-      box-shadow: 0 2px 4px rgba(0,0,0,0.1);
-      display: flex; 
-      align-items: center; 
-      justify-content: flex-start; 
-      position: fixed; 
-      top: 0; 
-      left: 0;
-    ">
-      <p v-for="route in routes" 
-         :key="route.path" 
-         @click="handleLinkClick(route.path)" 
-         style="
-           cursor: pointer; 
-           margin-right: 20px;
-           font-size: 16px;
-           font-weight: bold;
-           transition: color 0.3s;
-         "
-         :style="{ color: currentPath.value === route.path ? '#4CAF50' : 'inherit' }"
-         >
+    <nav
+      style="
+        height: 60px;
+        width: 100%;
+        padding: 0 20px;
+        background-color: #4caf50;
+        color: white;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        position: fixed;
+        top: 0;
+        left: 0;
+      "
+    >
+      <p
+        v-for="route in routes"
+        :key="route.path"
+        @click="handleLinkClick(route.path)"
+        style="
+          height: calc(100% - 10px);
+          display: flex;
+          align-items: center;
+          cursor: pointer;
+          margin-right: 20px;
+          font-size: 16px;
+          font-weight: bold;
+          transition: color 0.3s;
+          padding: 0 10px;
+          font-family: SourceHanSansCN-Regular;
+        "
+        :style="{
+          color: currentPath === route.path ? '#fff' : '#ffffffe0',
+          borderBottom: currentPath === route.path ? '3px solid #fff' : 'none',
+        }"
+      >
         {{ route.name }}
       </p>
     </nav>
-    <main class="route" style="
-      
-    ">
-       <component :is="currentComponent"></component>
+    <main class="route" style="">
+      <component :is="currentComponent"></component>
     </main>
   </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, watchEffect, markRaw  } from 'vue'
-import UsePdf from './views/UsePdf.vue'
-import NotFound from './views/NotFound.vue';
-import Home from './views/Home.vue';
+import { onMounted, ref, watchEffect, markRaw } from "vue";
+import UsePdf from "./views/UsePdf.vue";
+import NotFound from "./views/NotFound.vue";
+import Home from "./views/Home.vue";
+import UsePdfOnDemand from "./views/UsePdfOnDemand.vue";
 
 const routes = [
-  { path: '/', name: '主页',component: markRaw(Home) },
-  { path: '/usePdf', name: '使用pdf', component: markRaw(UsePdf) },
-  { path: '/ondemandRenderPdf', name: '按需渲染pdf', component: markRaw(UsePdf) },
-  { path: '/notFound', component: markRaw(NotFound) },
+  { path: "/", name: "使用iframe加载pdf", component: markRaw(Home) },
+  { path: "/usePdf", name: "使用pdfjs-dist", component: markRaw(UsePdf) },
+  {
+    path: "/onDemandRenderPdf",
+    name: "按需渲染pdf",
+    component: markRaw(UsePdfOnDemand),
+  },
+  { path: "/notFound", component: markRaw(NotFound) },
 ];
 
-const currentPath = ref('/usePdf');
+const currentPath = ref("/usePdf");
 const currentComponent = ref(NotFound);
 
 const getComponentByPath = (path: string) => {
-  return routes.find(route => route.path === path) || { component: NotFound };
-}
+  return routes.find((route) => route.path === path) || { component: NotFound };
+};
 
 onMounted(() => {
   currentComponent.value = getComponentByPath(currentPath.value).component;
@@ -70,23 +83,23 @@ watchEffect(() => {
 // 你还可以添加一个函数来处理链接点击，例如:
 function handleLinkClick(path: string) {
   currentComponent.value = getComponentByPath(path).component;
+  currentPath.value = path;
 }
-
 </script>
 
-<style lang="css" scoped >
-
+<style lang="css" scoped>
 .route {
-  width: 1000px; 
-  margin: 60px auto 0; 
-  overflow-y: auto;
-  min-height: calc(100vh - 60px); /* Full viewport height minus the height of the nav bar */
-  box-shadow: 0 2px 10px rgba(0,0,0,0.1);
+  width: 1000px;
+  margin: 60px auto 0;
+  height: calc(
+    100vh - 80px
+  ); /* Full viewport height minus the height of the nav bar */
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
   background-color: #f8f8f8;
   scrollbar-width: thin;
-  scrollbar-color: #4CAF50 #f8f8f8;
+  scrollbar-color: #4caf50 #f8f8f8;
 }
- /* For Webkit browsers */
+/* For Webkit browsers */
 .route::-webkit-scrollbar {
   width: 8px;
 }
@@ -94,7 +107,7 @@ function handleLinkClick(path: string) {
   background: #f8f8f8;
 }
 .route::-webkit-scrollbar-thumb {
-  background-color: #4CAF50;
+  background-color: #4caf50;
   border-radius: 10px;
   border: 2px solid #f8f8f8;
 }
--- src/views/UsePdfOnDemand.vue
+++ src/views/UsePdfOnDemand.vue
@@ -1,108 +1,146 @@
 <template>
-  <div class="ondemand-pdf-container" ref="pdfContainerRef">
-    <canvas v-for="pageIndex in pdfPages" :id="`pdf-canvas-${pageIndex}`"  :key="pageIndex" />
+  <div class="on-demand-pdf-container" ref="pdfContainerRef">
+    <canvas
+      v-for="pageIndex in renderedPages"
+      :id="`pdf-canvas-${pageIndex}`"
+      :key="pageIndex"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
-import * as PDFJS from 'pdfjs-dist'
-import * as PdfWorker from 'pdfjs-dist/build/pdf.worker.js'
-import { nextTick, onMounted, ref } from 'vue'
-import PdfBook from '@/assets/JavaScrip.pdf'
+// import * as PDFJS from "pdfjs-dist";
+// import * as PdfWorker from "pdfjs-dist/build/pdf.worker.js";
+import { nextTick, onMounted, ref, computed, onUnmounted } from "vue";
+import PdfBook from "@/assets/JavaScript.pdf";
 
-window.pdfjsWorker = PdfWorker
-let pdfDoc:any = null
-const pdfPages = ref(0)
-const pdfScale = ref(1.5)
-const pdfContainerRef = ref<HTMLElement | null>(null)
+let pdfDoc: any = null;
+const pdfPages = ref(0);
+const pdfScale = ref(1.5);
+const pdfContainerRef = ref<HTMLElement | null>(null);
+const loadedNum = ref(0);
+const preloadNum = computed(() => {
+  return pdfPages.value - loadedNum.value > 3
+    ? 3
+    : pdfPages.value - loadedNum.value;
+});
+const loadFished = computed(() => {
+  const loadFinished = loadedNum.value + preloadNum.value >= pdfPages.value;
+  if (loadFinished) {
+    removeEventListeners();
+  }
+  return loadFinished;
+});
 
-PDFJS.GlobalWorkerOptions.workerSrc = PdfWorker
-let loadingTask
+const renderedPages = computed(() => {
+  return loadFished.value ? pdfPages.value : loadedNum.value + preloadNum.value;
+});
 
-const loadFile = (url:any) => {
-  // 设定pdfjs的 workerSrc 参数
-  PDFJS.GlobalWorkerOptions.workerSrc = PdfWorker
-  const loadingTask = PDFJS.getDocument(url)
-  loadingTask.promise.then(async (pdf:any) => {
-    pdf.loadingParams.disableAutoFetch = true
-    pdf.loadingParams.disableStream = true
-    pdfDoc = pdf // 保存加载的pdf文件流
-    pdfPages.value = pdfDoc.numPages // 获取pdf文件的总页数
-    await nextTick(() => {
-      renderPage(1) // 将pdf文件内容渲染到canvas
-    })
-  }).catch((error:any) => {
-    console.warn(`[upthen] pdfReader loadFile error: ${error}`)
-  })
-}
-
-
-
-const renderPage = (num:any) => {
-  pdfDoc.getPage(num).then((page:any) => {
-    page.cleanup()
+let loadingTask;
+const renderPage = (num: any) => {
+  pdfDoc.getPage(num).then((page: any) => {
+    page.cleanup();
     if (pdfContainerRef.value) {
-      pdfScale.value = pdfContainerRef.value.clientWidth / page.view[2]
+      pdfScale.value = pdfContainerRef.value.clientWidth / page.view[2];
     }
-    const canvas:any = document.getElementById(`pdf-canvas-${num}`)
+    const canvas: any = document.getElementById(`pdf-canvas-${num}`);
     if (canvas) {
-      const ctx = canvas.getContext('2d')
-      const dpr = window.devicePixelRatio || 1
-      const bsr = ctx.webkitBackingStorePixelRatio ||
-                  ctx.mozBackingStorePixelRatio ||
-                  ctx.msBackingStorePixelRatio ||
-                  ctx.oBackingStorePixelRatio ||
-                  ctx.backingStorePixelRatio ||
-                  1
-      const ratio = dpr / bsr
-      const viewport = page.getViewport({ scale: pdfScale.value })
-      canvas.width = viewport.width * ratio
-      canvas.height = viewport.height * ratio
-      canvas.style.width = viewport.width + 'px'
-      canvas.style.height = viewport.height + 'px'
-      ctx.setTransform(ratio, 0, 0, ratio, 0, 0)
+      const ctx = canvas.getContext("2d");
+      const dpr = window.devicePixelRatio || 1;
+      const bsr =
+        ctx.webkitBackingStorePixelRatio ||
+        ctx.mozBackingStorePixelRatio ||
+        ctx.msBackingStorePixelRatio ||
+        ctx.oBackingStorePixelRatio ||
+        ctx.backingStorePixelRatio ||
+        1;
+      const ratio = dpr / bsr;
+      const viewport = page.getViewport({ scale: pdfScale.value });
+      canvas.width = viewport.width * ratio;
+      canvas.height = viewport.height * ratio;
+      canvas.style.width = viewport.width + "px";
+      canvas.style.height = viewport.height + "px";
+      ctx.setTransform(ratio, 0, 0, ratio, 0, 0);
       const renderContext = {
         canvasContext: ctx,
-        viewport: viewport
+        viewport: viewport,
+      };
+      page.render(renderContext);
+      if (num < loadedNum.value + preloadNum.value && !loadFished.value) {
+        renderPage(num + 1);
+      } else {
+        loadedNum.value = loadedNum.value + preloadNum.value;
       }
-      page.render(renderContext)
-      // if (num < pdfPages.value) {
-      //   renderPage(num + 1)
-      // }
     }
-  })
-}
+  });
+};
 
-const initPdfLoader = (loadingTask: any) => {
+const initPdfLoader = async (loadingTask: any) => {
   return new Promise((resolve, reject) => {
-    loadingTask.promise.then((pdf: any) => {
-      pdf.loadingParams.disableAutoFetch = true
-      pdf.loadingParams.disableStream = true
-      pdfDoc = pdf // 保存加载的pdf文件流
-      pdfPages.value = pdfDoc.numPages // 获取pdf文件的总页数
-      resolve(true)
-    }).catch((error:any) => {
-      reject(error)
-      console.warn(`[upthen] pdfReader loadFile error: ${error}`)
-    })
-  })
-}
+    loadingTask.promise
+      .then((pdf: any) => {
+        pdf.loadingParams.disableAutoFetch = true;
+        pdf.loadingParams.disableStream = true;
+        pdfDoc = pdf; // 保存加载的pdf文件流
+        pdfPages.value = pdfDoc.numPages; // 获取pdf文件的总页数
+        resolve(true);
+      })
+      .catch((error: any) => {
+        reject(error);
+        console.warn(`[upthen] pdfReader loadFile error: ${error}`);
+      });
+  });
+};
+
+const distanceToBottom = ref(0);
+const calculateDistanceToBottom = () => {
+  if (pdfContainerRef.value) {
+    const containerHeight = pdfContainerRef.value.offsetHeight;
+    const containerScrollHeight = pdfContainerRef.value.scrollHeight;
+    distanceToBottom.value =
+      containerScrollHeight - containerHeight - pdfContainerRef.value.scrollTop;
+    console.log(distanceToBottom.value);
+  }
+};
+
+const lazyRenderPdf = () => {
+  calculateDistanceToBottom();
+  if (distanceToBottom.value < 1000) {
+    renderPage(loadedNum.value);
+  }
+};
+
+const removeEventListeners = () => {
+  pdfContainerRef.value?.removeEventListener("scroll", () => {
+    lazyRenderPdf();
+  });
+};
 
 onMounted(async () => {
-  // loadFile(PdfBook)
   // 设定pdfjs的 workerSrc 参数
-  
-  loadingTask = PDFJS.getDocument(PdfBook)
-
+  let PDFJS = await import("pdfjs-dist");
+  window.pdfjsWorker = await import("pdfjs-dist/build/pdf.worker.js");
+  PDFJS.GlobalWorkerOptions.workerSrc = window.pdfjsWorker;
+  loadingTask = PDFJS.getDocument(PdfBook);
   if (await initPdfLoader(loadingTask)) {
-    renderPage(1)
+    renderPage(1);
   }
 
-})
+  pdfContainerRef.value.addEventListener("scroll", () => {
+    lazyRenderPdf();
+  });
+});
+
+onUnmounted(() => {
+  removeEventListeners();
+});
 </script>
 
 <style lang="scss" scoped>
-.ondemand-pdf-container  {
+.on-demand-pdf-container {
+  height: 100%;
+  overflow-y: scroll;
+  overflow-x: hidden;
   canvas {
     display: flex;
     flex-direction: column;
